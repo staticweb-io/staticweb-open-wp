30825b1c2e9a084fbe25bd2e9be36d474ce5c671
John Shaffer <jshaffer2112@protonmail.com>
2021-05-09 23:30:56 -0500

    Add a user ns that requires everything else.
    This makes it easy to check that everything compiles.

---
+++ static-wp-daemon/src/io/staticweb/static_wp_daemon/user.clj
@@ -0,0 +1,6 @@
+(ns io.staticweb.static-wp-daemon.user
+  (:require io.staticweb.static-wp-daemon.init-deploy
+            io.staticweb.static-wp-daemon.mysql
+            io.staticweb.static-wp-daemon.set-server-name
+            io.staticweb.static-wp-daemon.shutdown
+            io.staticweb.static-wp-daemon.util))
